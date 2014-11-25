12bed5f833b22dd24a6fbe63335986c8bfb95a1d
Dawei Yang <davy.pristina@gmail.com>
2014-11-25 23:45:14 +0800

    Version 1.0

---
+++ src/com/nutcake/visualsocial/SocialVisualizer.java
@@ -0,0 +1,54 @@
+package com.nutcake.visualsocial;
+
+import com.renren.api.RennClient;
+import com.renren.api.RennException;
+import com.renren.api.service.User;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Dawei YANG
+ * @since 11/25/14
+ */
+public class SocialVisualizer {
+    public static void main(String[] args) throws RennException {
+
+        String apiKey, apiSecret;
+
+        // 读取应用配置信息
+        try {
+            Scanner scanner = new Scanner(new BufferedInputStream(new FileInputStream("conf/secret.txt")));
+            apiKey = scanner.nextLine();
+            apiSecret = scanner.nextLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // 初始化应用接口
+        RennClient renn = new RennClient(apiKey, apiSecret);
+        renn.authorizeWithClientCredentials();
+
+        // 测试用户
+        long daweiId = 356795949L;
+        User user = renn.getUserService().getUser(daweiId);
+        System.out.println(user.getName() + "'s friend: (10)");
+
+        //显示10个好友
+        Integer[] friendsId = renn.getFriendService().listFriend(daweiId, 10, 1);
+
+        List<String> names = new ArrayList<>();
+        for (Integer i : friendsId) {
+            User friend = renn.getUserService().getUser(i.longValue());
+            names.add(friend.getName());
+        }
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
--- src/renren/Renren.java
+++ src/renren/Renren.java
@@ -15,7 +15,9 @@ public class Renren {
     /**
      * @param args the command line arguments
      */
+    /**
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+     */
 }
