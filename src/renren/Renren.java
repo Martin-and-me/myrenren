bb6037faf8b50e91a024fa341793b9bff51ad091
Zhang Yaoyang <330625727@qq.com>
2014-11-25 21:49:31 +0800

    Hello world!

--- src/renren/Renren.java
+++ src/renren/Renren.java
@@ -16,7 +16,6 @@ public class Renren {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world!");
     }
-    
 }
